8b44d00c226cb19815985d02a6569e7f52801e61
Miguel Dumetz <migueldumetz@gmail.com>
2026-05-25 08:38:54 -0400

    prisma fix

--- electron/main/server/db.ts
+++ electron/main/server/db.ts
@@ -1,13 +1,8 @@
 import { PrismaClient } from '@prisma/client'
 import { join } from 'path'
 
-function getDbPath(): string {
-  return join(process.cwd(), 'prisma', 'dev.db')
-}
-
 const prisma = new PrismaClient({
-  datasources: { db: { url: `file:${getDbPath()}` } },
+  datasources: { db: { url: `file:${join(process.cwd(), 'prisma', 'dev.db')}` } },
 })
-
 
 export default prisma
